673974dc500e76bc71011cbdf1b58ec49d1b705c
Alexander Glering <algle11@student.sdu.dk>
2022-12-07 00:43:46 +0100

    Prettier

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,43 +1,43 @@
-import React from 'react';
-import './Header.scss';
-import { motion } from 'framer-motion';
-import { images } from '../../constants'; 
+import React from "react";
+import "./Header.scss";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div className="app__header app__flex">
       <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0,1]}}
-      transition= {{ duration: 0.5 }}
-      className='app__header-info'>
-        <div className='app__header-badge'>
-          <div className='badge-cmp app__flex'>
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className="app__header-info"
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
             <span>👋</span>
-          <div style={{ marginLeft: 20}}>
-            <p className='p-text'>Hi, I'm</p>
-            <h1 className='head-text'>Your dad</h1>
-          </div>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hi, I'm</p>
+              <h1 className="head-text">Your dad</h1>
+            </div>
           </div>
 
-          <div className='tag-cmp app__flex'>
-            <p className='p-text'>Big Cheese</p>
-            <p className='p-text'>Don Of The Underworld</p>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Big Cheese</p>
+            <p className="p-text">Don Of The Underworld</p>
           </div>
         </div>
       </motion.div>
 
       <motion.div
-        whileInView={{ opacity: [0,1]}}
-        transition={{ duration: 0.5, delayChildren: 0.5}}
-        className='app__header-img'>
-        <img src={images.profile} alt='profile_bg'/>
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile_bg" />
       </motion.div>
 
-      <motion.div>
+      <motion.div></motion.div>
+    </div>
+  );
+};
 
-      </motion.div>
-  </div>
-  )
-}
-
-export default Header
+export default Header;
